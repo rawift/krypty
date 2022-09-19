e0d95b9cdf54e501fb21d08deea9150ede3d58a1
rawift <102667382+rawift@users.noreply.github.com>
2022-09-19 20:04:42 +0530

    new

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,4 @@
-// https://eth-goerli.g.alchemy.com/v2/gLBKqBVvZ2XC9lI_DtfBmJ4zmdGxbSiq
+
 
 require('@nomiclabs/hardhat-waffle');
 
@@ -6,7 +6,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     Goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/gLBKqBVvZ2XC9lI_DtfBmJ4zmdGxbSiq',
+      url: 'bla bla write your own ',
       accounts: ['4fd878751cc0c30a428f6ab289f8bf52b8a72d4668622784dd15b4e074b77547']
     }
   }
